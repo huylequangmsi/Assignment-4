3a6d36252f386f807d1568f72451cee64705a352
Huy Le Quang <huy.le@gmx.de>
2020-07-27 17:05:24 +0200

    Add democracy

--- Data_cleaning.R
+++ Data_cleaning.R
@@ -27,6 +27,41 @@ save(data_final, file = "data_final.RData")
 
 country.name <- setNames(data_final$country, data_final$country) # country names
 
+## Attitude to Democracy
+
+democracy <- data_final %>% 
+    group_by(country) %>% 
+    summarise(m.V228A = mean(V228A, na.rm = TRUE),
+              m.V228B = mean(V228B, na.rm = TRUE),
+              m.V228C = mean(V228C, na.rm = TRUE),
+              m.V228D = mean(V228D, na.rm = TRUE),
+              m.V228E = mean(V228E, na.rm = TRUE),
+              m.V228F = mean(V228F, na.rm = TRUE),
+              m.V228G = mean(V228G, na.rm = TRUE),
+              m.V228H = mean(V228H, na.rm = TRUE),
+              m.V228I = mean(V228I, na.rm = TRUE))
+head(democracy)
+
+democracy[complete.cases(democracy),] %>% 
+    ggplot(aes(x = country))+
+    geom_point(aes(y = m.V228A, colour = "Votes are counted fairly"))+
+    geom_point(aes(y = m.V228B, colour = "Opposition candidates are prevented from running"))+
+    geom_point(aes(y = m.V228C, colour = "TV news favors the governing party"))+
+    geom_point(aes(y = m.V228D, colour = "Voters are bribed"))+
+    geom_point(aes(y = m.V228E, colour = "Journalists provide fair coverage of elections"))+
+    geom_point(aes(y = m.V228F, colour = "Election officials are fair"))+
+    geom_point(aes(y = m.V228G, colour = "Rich people buy elections"))+
+    geom_point(aes(y = m.V228H, colour = "Voters are threatened with violence"))+
+    geom_point(aes(y = m.V228I, colour = "Voters are offered a genuine choice"))+
+    theme(axis.text.x = element_text(angle = 90, hjust = 1), 
+          text = element_text(size = 9)) + 
+    xlab("Country") + 
+    ylab("Mean Reported Attitude to Science") + 
+    labs(title = "Self-reported Attitude to Science", 
+         subtitle = "(1: Very often, 4: Not at all often)",
+         caption = "Source: World Values Survey, Wave 6, 2010-14")
+    
+
 ## Attitude to Science 
 
 science <- data_final %>% 
@@ -53,7 +88,7 @@ science[complete.cases(science),] %>%
     xlab("Country") + 
     ylab("Mean Reported Attitude to Science") + 
     labs(title = "Self-reported Attitude to Science", 
-         subtitle = "(1: Completely disagree, 10: Completely agree",
+         subtitle = "(1: Completely disagree, 10: Completely agree)",
          caption = "Source: World Values Survey, Wave 6, 2010-14")
 
 
